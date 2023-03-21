28644b85c34d38b8f1a6fa17a8ddef7f668bafe7
ChristianMendez1 <ChristianMendez1213@gmail.com>
2023-03-21 15:26:56 -0400

    second commit

--- script.js
+++ script.js
@@ -17,32 +17,65 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // Do all of your work inside the document.addEventListener  
 
   // Part 1
-
+document.getElementById('main-title').innerHTML = 'DOM CHRISTIAN HOMEPAGE';
 
   // Part 2
-
+document.querySelector('body').style.backgroundColor = 'lightblue'
 
   // Part 3
-
+let removeLi = document.getElementById('favorite-things');
+removeLi.removeChild(removeLi.lastElementChild);
 
   // Part 4
-
+// let something = document.getElementsByClassName("special-title")
+// something.innerHTML = "abc"
+const fontSizeChange = document.querySelectorAll('.special-title');
+fontSizeChange.forEach((fontSizeChange) => {
+  fontSizeChange.style.fontSize = '2rem';
+});
 
   // Part 5
+  let removePR = document.getElementById('past-races');
 
+  if (removePR.hasChildNodes()) {
+    removePR.removeChild(removePR.children[3]);
+  }
 
   // Part 6
-
+  let newList = document.createElement("li");
+  newList.innerHTML = "New York City";
+  document.getElementById('past-races').appendChild(newList);
 
   // Part 7
+  let newDiv = document.createElement("div");
+  newDiv.classList.add('blog-post');
+  newDiv.classList.add('purple');
 
+  let getBody = document.querySelector('body')
+  getBody.children[2].append(newDiv);
+
+  let newH2 = document.createElement('h1');
+  newH2.innerHTML = "New York City";
+  newDiv.appendChild(newH2);
+
+  let newP = document.createElement('p');
+  newP.innerHTML = 'I DRAG RACED ACROSS THE BROOKLYN BRIDGE'
+  newDiv.appendChild(newP);
 
   // Part 8
-
+ let qotClick = document.getElementById("quote-title");
+ qotClick.addEventListener('click',randomQuote);
 
   // Part 9
-
-
-
-
+  const blogPostAll = document.querySelectorAll('.blog-post');
+  blogPostAll.forEach((blogPostAll) => {
+    blogPostAll.addEventListener('mouseout', mouseOut)
+    blogPostAll.addEventListener('mouseenter', mouseEnter)
+    function mouseOut() {
+      blogPostAll.classList.remove('red');
+    }
+    function mouseEnter() {
+      blogPostAll.classList.toggle('red');
+    }
+  });
 });
